1f65f232e7ff96b28bbfd0aca2aa36341efdd7ee
Karamandal <acandreyan@gmail.com>
2024-03-10 23:11:45 +0700

    14.1 Первая версия

--- src/classes.py
+++ src/classes.py
@@ -26,6 +26,12 @@ class Category:
             products_list.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity_in_stock} шт.")
         return products_list
 
+    def __len__(self):
+        return sum(product.quantity_in_stock for product in self.__products)
+
+    def __str__(self):
+        return f"{self.name}, количество продуктов: {len(self)} шт."
+
 
 class Product:
     def __init__(self, name: str, description: str, price: float, quantity_in_stock: int):
@@ -33,6 +39,9 @@ class Product:
         self.description = description
         self.__price = price
         self.quantity_in_stock = quantity_in_stock
+
+    def __str__(self):
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity_in_stock} шт."
 
     @classmethod
     def create_new_product(cls, product_data):
@@ -55,3 +64,8 @@ class Product:
                 print("Изменение цены отменено")
         else:
             self.__price = new_price
+
+    def __add__(self, other):
+        total_price = (self.price * self.quantity_in_stock) + (other.price * other.quantity_in_stock)
+        return total_price
+
